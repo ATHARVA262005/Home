e7f071352b21b27f90694593e491f7e48fd58a5d
Atharva Ralegankar <112944002+ATHARVA262005@users.noreply.github.com>
2023-12-24 11:18:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#heroMsg', {
+    strings: ['Step into a world where imagination knows no bounds', ' where colors and concepts collide','where creativity finds its truest expression'],
+    typeSpeed: 50,
+    backSpeed: 25,
+    loop :true,
+  });
