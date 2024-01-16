aa8bd21facde5d76e3fd130759a6972936c174b6
Atharva Ralegankar <112944002+ATHARVA262005@users.noreply.github.com>
2024-01-16 23:54:49 +0530

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed('#heroMsg', {
-    strings: ['Step into a world where imagination knows no bounds', ' where colors and concepts collide','where creativity finds its truest expression'],
-    typeSpeed: 50,
-    backSpeed: 25,
-    loop :true,
-  });
